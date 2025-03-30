6840f6c5f2342c3eb413196519dd483845cd263b
philipbroadway <me@philipbroadway.com>
2025-03-30 09:31:53 -0600

    updated example

--- example/api/src/main.rs
+++ example/api/src/main.rs
@@ -1,6 +1,141 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
 use patina::Patina;
-use serde::Deserialize;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(list)
+            .service(get_book)
+            .service(get_chapter)
+            .service(get_verse)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+static STYLE: &str = r#"
+<style>
+  body {
+    font-family: sans-serif;
+    margin: 0;
+    padding: 1em;
+    line-height: 1.3;
+  }
+  a {
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  h2 {
+    margin-bottom: 0;
+    padding: 0;
+  }
+  hr {
+    border-top: 0.5px solid #ccc;
+  }
+</style>
+"#;
+
+#[get("/")]
+async fn list() -> impl Responder {
+    let patina = Patina::new();
+    let books = patina.list_books();
+    let body = books
+        .iter()
+        .map(|book| format!(r#"<a href="/{}">{}</a>"#, book, kebab_to_title_case(book)))
+        .collect::<Vec<_>>()
+        .join("<br/>");
+
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(format!(
+          r#"
+            <html>
+              <head>
+                {}
+              </head>
+              <body>
+                <h2>{}</h2>
+                <hr />
+                {}
+              </body>
+            </html>
+          "#,STYLE, "The Bible", body))
+}
+
+
+#[get("/{book}")]
+async fn get_book(path: web::Path<String>) -> impl Responder {
+    let patina = Patina::new();
+    let book = path.into_inner();
+    let chapters = patina.list_chapters(&book).unwrap_or_default();
+
+    let body = chapters
+        .iter()
+        .enumerate()
+        .map(|(i, _)| {
+            let chapter_num = i + 1;
+            format!(r#"<a href="/{}/{}">Chapter {}</a>"#, book, chapter_num, chapter_num)
+        })
+        .collect::<Vec<_>>()
+        .join("<br/>");
+
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(format!(
+          r#"
+            <html>
+              <head>
+                {}
+              </head>
+              <body>
+                <h2><a href="/">{}</a></h2>
+                <hr />
+                {}
+              </body>
+            </html>
+          "#,STYLE, kebab_to_title_case(&book), body)
+        )
+}
+
+
+#[get("/{book}/{chapter}")]
+async fn get_chapter(path: web::Path<(String, u32)>) -> impl Responder {
+  let patina = Patina::new();
+  let (book, chapter) = path.into_inner();
+
+  let verses = patina.list_verses(&book, chapter).unwrap_or_default();
+
+  let body = verses
+      .iter()
+      .enumerate()
+      .map(|(i, _)| {
+          let verse_num = i + 1;
+          format!(r#"<a href="/{}/{}/{}">Verse {}</a>"#, book, chapter, verse_num, verse_num)
+      })
+      .collect::<Vec<_>>()
+      .join("<br/>");
+
+  HttpResponse::Ok()
+      .content_type("text/html; charset=utf-8")
+      .body(format!(
+        r#"
+          <html>
+            <head>
+              {}
+            </head>
+            <body>
+              <h2><a href="/{}">{} {}</a></h2>
+              <hr />
+              {}
+            </body>
+          </html>
+        "#,STYLE, &book, kebab_to_title_case(&book), chapter, body)
+      )
+}
 
 #[get("/{book}/{chapter}/{verse}")]
 async fn get_verse(path: web::Path<(String, String, String)>) -> impl Responder {
@@ -24,73 +159,38 @@ async fn get_verse(path: web::Path<(String, String, String)>) -> impl Responder 
         }
     };
 
-    // Pass book as &str, and chapter/verse as Option<u32>
-    let result = Patina::new()
+    let body = Patina::new()
         .search_by_reference(&book, chapter, verse)
         .unwrap_or_else(|| "Reference not found.".to_string());
 
-    HttpResponse::Ok().body(result)
+    let chapter_display = chapter.map(|ch| ch.to_string()).unwrap_or_default();
+    let verse_display = verse.map(|v| v.to_string()).unwrap_or_default();
+
+    HttpResponse::Ok()
+    .content_type("text/html; charset=utf-8")
+    .body(format!(
+      r#"
+        <html>
+          <head>
+            {}
+          </head>
+          <body>
+            {}<hr/><span><a href="/{}">{}</a> <a href="/{}/{}">{}:{}</a></span>
+          </body>
+        </html>
+      "#,STYLE, body, &book, kebab_to_title_case(&book), &book, chapter_display, chapter_display, verse_display)
+    )
 }
 
-
-#[derive(Deserialize)]
-struct SearchQuery {
-    q: String,
-}
-
-#[get("/")]
-async fn search(q: web::Query<SearchQuery>) -> impl Responder {
-    let search_query = &q.q;
-
-    let result = Patina::new()
-        .search_by_text(search_query)
-        .unwrap_or_else(|| "No results found".to_string());
-
-    HttpResponse::Ok().body(result)
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(get_book) // Route for /{book}
-            .service(get_chapter) // Route for /{book}/{chapter}  
-            .service(get_verse) // Route for /{book}/{chapter}/{verse}
-            .service(search) // Route for /?query=text
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-}
-
-#[get("/{book}/{chapter}")]
-async fn get_chapter(path: web::Path<(String, String)>) -> impl Responder {
-    let (book, chapter) = path.into_inner();
-
-    // Parse chapter as Option<u32>
-    let chapter: Option<u32> = if chapter.is_empty() {
-        None
-    } else {
-        match chapter.parse() {
-            Ok(num) => Some(num),
-            Err(_) => return HttpResponse::BadRequest().body("Invalid chapter format"),
-        }
-    };
-
-    let result = Patina::new()
-        .search_by_reference(&book, chapter, None)
-        .unwrap_or_else(|| "Reference not found.".to_string());
-
-    HttpResponse::Ok().body(result)
-}
-
-#[get("/{book}")]
-async fn get_book(path: web::Path<String>) -> impl Responder {
-    let book = path.into_inner();
-
-    let result = Patina::new()
-        .search_by_reference(&book, None, None)
-        .unwrap_or_else(|| "Reference not found.".to_string());
-
-    HttpResponse::Ok().body(result)
+fn kebab_to_title_case(s: &str) -> String {
+  s.split('-')
+      .map(|word| {
+          let mut c = word.chars();
+          match c.next() {
+              Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
+              None => String::new(),
+          }
+      })
+      .collect::<Vec<_>>()
+      .join(" ")
 }
